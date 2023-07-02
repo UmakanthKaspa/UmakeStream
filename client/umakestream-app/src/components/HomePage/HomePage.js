01126fcda0e93ec867290905cc8be10b036442a4
Umakanthkaspa <kaspaumakanth1999@gmail.com>
2023-07-02 09:28:30 +0530

    feat: Add authentication and authorization functionality

--- client/umakestream-app/src/components/HomePage/HomePage.js
+++ client/umakestream-app/src/components/HomePage/HomePage.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie'
 
-export default function HomePage() {
+const Home = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const jwtToken = Cookies.get('jwt_token')
+      const response = await fetch('http://localhost:5000/api/home', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.message);
+      } else {
+        console.log('Authentication failed');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h2>Home Page</h2>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Home;
--- client/umakestream-app/src/components/SignupPage/SignupPage.js
+++ client/umakestream-app/src/components/SignupPage/SignupPage.js
@@ -1,89 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom';
+import "./SignupPage.css";
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    email: "",
+    password: "",
   });
+
+  useEffect(() => {
+    const jwtToken = Cookies.get('jwt_token');
+    if (jwtToken !== undefined) {
+      navigate('/');
+    }
+  }, [navigate]);
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const success = (jwtToken) => {
+    Cookies.set('jwt_token', jwtToken, { expires: 30 });
+    navigate('/');
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/signup', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5000/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
-
+      const data = await response.json();
       if (response.ok) {
         // Signup successful
-        console.log('Signup successful');
-        // Redirect or perform desired actions
+        console.log("Signup successful");
+        success(data.token);
       } else {
         // Signup failed
-        const errorData = await response.json();
-        console.log('Signup failed:', errorData.error);
+        console.log("Signup failed:", data.error);
       }
     } catch (error) {
-      console.error('An error occurred during signup:', error);
+      console.error("An error occurred during signup:", error);
     }
   };
 
   return (
-    <div>
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
+    <div className="signup-container">
+      <div className="signup-card">
+        <h1>Unlimited movies, TV</h1>
+        <h1>shows and more.</h1>
+        <h4>Watch anywhere. Cancel anytime.</h4>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Signup</button>
+        </form>
+        <h6>
+          Ready to watch? Enter your email to create or restart membership.
+        </h6>
+      </div>
     </div>
   );
 };
